f5f9277d34c051b0085339e7253b8b3bb4d8e53b
vishnu vinod <vishnujishnu0987@gmail.com>
2026-03-23 11:21:55 +0530

    hot fix

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,6 @@
 mod storage;
 mod runtime;
 
-use runtime::EventLoop;
-// use storage::Storage;
-
 fn main(){
-    let mut eventLoop = EventLoop::new();
-    
-    eventLoop.add_tasks("716253716253761253", 123);
-    eventLoop.add_tasks("444", 39999);
-    eventLoop.add_tasks("716253716253761253", 212123);
-    eventLoop.add_tasks("716253716253761253", 1);
-    eventLoop.add_tasks("716253716253761253", 2);
-
-    eventLoop.run();
 
 }
